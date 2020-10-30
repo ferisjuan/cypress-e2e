13ff290eb86387b8e191951f65ad2e7f249c80c8
juanferis <jferisgo@directvla.com.ar>
2020-10-30 14:51:24 -0500

    Test: Password

---
+++ cypress/integration/e2e_tests/password.spec.js
@@ -0,0 +1,21 @@
+describe('Password tests', () => {
+	before(function () {
+		cy.visit('http://zero.webappsecurity.com/')
+	})
+
+	it('should click on the signin button', () => {
+		cy.get('#signin_button').click()
+	})
+
+	it('should click on the forgoten password', () => {
+		cy.get('.offset3 > a').click()
+	})
+
+	it('should fill email form', () => {
+		cy.get('#user_email').type('test.email@email.com')
+	})
+
+	it('should submit the form', () => {
+		cy.contains('Send Password').click()
+	})
+})
